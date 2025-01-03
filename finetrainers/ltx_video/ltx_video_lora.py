133baee55d2279b292937c52879cad20042c129b
sayakpaul <spsayakpaul@gmail.com>
2025-01-03 20:08:37 +0530

    revert changes

--- finetrainers/ltx_video/ltx_video_lora.py
+++ finetrainers/ltx_video/ltx_video_lora.py
@@ -238,7 +238,6 @@ def validation(
         "frame_rate": frame_rate,
         "num_videos_per_prompt": num_videos_per_prompt,
         "generator": generator,
-        "fps": kwargs.get("fps", None),
         "return_dict": True,
         "output_type": "pil",
     }
