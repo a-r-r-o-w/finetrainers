299e984721236de37c9cd1874f405600e0ec2979
Viveka Kulharia <vivekakulharia@gmail.com>
2025-03-02 04:45:04 -0800

    save_model_card function is only called if self.args.push_to_hub is True

--- finetrainers/trainer.py
+++ finetrainers/trainer.py
@@ -875,7 +875,7 @@ class Trainer:
 
         if num_validation_samples == 0:
             logger.warning("No validation samples found. Skipping validation.")
-            if accelerator.is_main_process:
+            if accelerator.is_main_process and self.args.push_to_hub:
                 save_model_card(
                     args=self.args,
                     repo_id=self.state.repo_id,
