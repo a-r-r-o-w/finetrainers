b2a8730b106579bf64cc0528623444f4d2578aa4
Viveka Kulharia <vivekakulharia@gmail.com>
2025-03-02 05:05:31 -0800

    saving the model card only when final_validation is True

--- finetrainers/trainer.py
+++ finetrainers/trainer.py
@@ -876,7 +876,7 @@ class Trainer:
         if num_validation_samples == 0:
             logger.warning("No validation samples found. Skipping validation.")
             if accelerator.is_main_process:
-                if self.args.push_to_hub:
+                if self.args.push_to_hub and final_validation:
                     save_model_card(
                         args=self.args,
                         repo_id=self.state.repo_id,
